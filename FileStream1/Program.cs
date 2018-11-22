8cfdc20df069e3221c61d0d5cb1fb820439845cd
Koson Trachu <koson.trachu@gmail.com>
2018-11-23 00:03:05 +0700

    Add project files.

---
+++ FileStream1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileStream1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\complab1\MyTest.txt";
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("Hello");
+                    sw.WriteLine("And");
+                    sw.WriteLine("Welcome");
+                }
+            }
+        }
+    }
+}
